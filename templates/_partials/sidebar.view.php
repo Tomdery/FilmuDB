cbc468f6eb005db47f070457c1f20bf8076d4a95
Tomdery <stunzenas.aironas@gmail.com>
2019-02-28 11:27:04 +0200

    Pushing the project

---
+++ templates/_partials/sidebar.view.php
@@ -0,0 +1,9 @@
+<div class="bg-light border-right" id="sidebar-wrapper">
+    <div class="sidebar-heading">Filmai </div>
+    <div class="list-group list-group-flush">
+        <a href="/zp181/forum_cinemas/?page=all-fils" class="list-group-item list-group-item-action bg-light">Visi filmai</a>
+        <a href="/zp181/forum_cinemas/?page=filter-by" class="list-group-item list-group-item-action bg-light">Filmai pagal žanrą</a>
+        <a href="/zp181/forum_cinemas/?page=search" class="list-group-item list-group-item-action bg-light">Filmo paieška</a>
+        <a href="/zp181/forum_cinemas/?page=stat" class="list-group-item list-group-item-action bg-light">Statistika</a>
+    </div>
+</div>
---
+++ templates/index.view.php
@@ -0,0 +1,60 @@
+<?php
+$siteName = 'Filmų duomenų bazė: ';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Filmu duomenu baze</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="d-flex" id="wrapper">
+
+    <?php include "_partials/sidebar.view.php"; ?>
+    <div id="page-content-wrapper">
+        <?php include "_partials/nav.view.php";?>
+
+        <div class="container-fluid">
+            <h1 class="mt-4"><?=$siteName; ?></h1>
+        </div>
+    </div>
+
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
+
+</body>
+
+</html>
+
+
+
